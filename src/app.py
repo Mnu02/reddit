70537832d373732b011ce43b77e81c829542594e
Mnumzana Moyo <mfm222@cornell.edu>
2025-03-24 01:37:13 -0400

    Initial commit

---
+++ src/app.py
@@ -0,0 +1,49 @@
+import json
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+# db
+posts = {
+    1: {
+      "id": 0,
+      "upvotes": 1,
+      "title": "My cat is the cutest!",
+      "link": "https://i.imgur.com/jseZqNK.jpg",
+      "username": "alicia98"
+    },
+    2: {
+      "id": 1,
+      "upvotes": 3,
+      "title": "Cat loaf",
+      "link": "https://i.imgur.com/TJ46wX4.jpg",
+      "username": "alicia98"
+    },
+    3: {}
+}
+
+@app.route("/")
+def hello_world():
+    return "Hello world!"
+
+
+# your routes here
+
+# create a post
+
+# get a specific post by id
+
+# delete a specific post by id
+
+# get comments for a specific post
+
+# post a comment for a specific post
+
+# edit a comment for a specific post
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
